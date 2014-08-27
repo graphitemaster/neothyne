fb2e3b5c2fb91a558b8f9b7a555c20c8093b72fb
Dale Weiler <weilercdale@gmail.com>
2014-08-27 02:52:18 -0400

    Simplyify raySphereInteresect

--- math.cpp
+++ math.cpp
@@ -48,28 +48,16 @@ namespace m {
     bool vec3::raySphereIntersect(const vec3 &start, const vec3 &direction,
         const vec3 &sphere, float radius, float *fraction)
     {
-        const vec3 end = start + direction;
-        // a
-        const float a = (end.x - start.x) * (end.x - start.x) +
-                        (end.y - start.y) * (end.y - start.y) +
-                        (end.z - start.z) * (end.z - start.z);
-        // b
-        const float b = 2.0f * ((end.x - start.x) * (start.x - sphere.x) +
-                                (end.y - start.y) * (start.y - sphere.y) +
-                                (end.z - start.z) * (start.z - sphere.z));
-        // c
-        const float c = sphere.x * sphere.x + sphere.y * sphere.y + sphere.z * sphere.z +
-                        start.x * start.x + start.y * start.y + start.z * start.z -
-                        2.0f * (sphere.x * start.x + sphere.y * start.y + sphere.z * start.z) - radius * radius;
-
-        // test
-        const float test = b * b - 4.0f * a * c;
-        if (test <= 0.0f)
+        const float a = direction.absSquared();
+        const vec3  s = start - sphere;
+        const float b = direction * s;
+        const float c1 = s.absSquared();
+        const float c4 = radius * radius;
+        const float c = c1 - c4;
+        const float t = b * b - a * c;
+        if (t <= 0.0f)
             return false;
-        const float d = sqrtf(test);
-        const float u1 = (-b + d) / (2.0f * a);
-        const float u2 = -(b + d) / (2.0f * a);
-        *fraction = u1 < u2 ? u1 : u2;
+        *fraction = -(b + fabs(sqrtf(t))) / a;
         return true;
     }
 
