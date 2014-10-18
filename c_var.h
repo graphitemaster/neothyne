168627aa139a5bf6b87d3a75382c3d6bc1a5b098
Dale Weiler <weilercdale@gmail.com>
2014-10-18 10:02:57 -0400

    Using this style because it makes the structure definition short and easy to read and understand simply by opening the header. The implementation details follow below instead

--- c_var.h
+++ c_var.h
@@ -12,12 +12,21 @@ enum varType {
 
 template <typename T>
 struct varTypeTraits;
+
 template <>
-struct varTypeTraits<int> { static constexpr varType type = VAR_INT; };
+struct varTypeTraits<int> {
+    static constexpr varType type = VAR_INT;
+};
+
 template <>
-struct varTypeTraits<float> { static constexpr varType type = VAR_FLOAT; };
+struct varTypeTraits<float> {
+    static constexpr varType type = VAR_FLOAT;
+};
+
 template <>
-struct varTypeTraits<u::string> { static constexpr varType type = VAR_STRING; };
+struct varTypeTraits<u::string> {
+    static constexpr varType type = VAR_STRING;
+};
 
 void varRegister(const char *name, const char *desc, void *what, varType type);
 
@@ -25,46 +34,13 @@ template <typename T>
 struct var {
     typedef void (*command)(T&);
 
-    var(const char *name, const char *desc, const T &min, const T &max, const T &def)
-        : m_min(min)
-        , m_max(max)
-        , m_default(def)
-        , m_current(def)
-        , m_callback(nullptr)
-    {
-        void *self = reinterpret_cast<void *>(this);
-        varRegister(name, desc, self, varTypeTraits<T>::type);
-    }
+    var(const char *name, const char *desc, const T &min, const T &max, const T &def);
+    var(const char *name, const char *desc, const T &min, const T &max, const T &def, command cb);
 
-    var(const char *name, const char *desc, const T &min, const T &max, const T &def, command cb)
-        : m_min(min)
-        , m_max(max)
-        , m_default(def)
-        , m_current(def)
-        , m_callback(cb)
-    {
-        void *self = reinterpret_cast<void *>(this);
-        varRegister(name, desc, self, varTypeTraits<T>::type);
-    }
-
-    operator T&() {
-        return m_current;
-    }
-
-    const T get() const {
-        return m_current;
-    }
-
-    void set(const T &value) {
-        if (value < m_min) return;
-        if (value > m_max) return;
-        m_current = value;
-    }
-
-    void operator()() {
-        if (m_callback)
-            m_callback(m_current);
-    }
+    operator T&();
+    const T get() const;
+    void set(const T &value);
+    void operator()();
 
 private:
     const T m_min;
@@ -78,46 +54,117 @@ template <>
 struct var<u::string> {
     typedef void (*command)(u::string &value);
 
-    var(const char *name, const char *desc, const u::string &def)
-        : m_default(def)
-        , m_current(def)
-        , m_callback(nullptr)
-    {
-        void *self = reinterpret_cast<void *>(this);
-        varRegister(name, desc, self, varTypeTraits<u::string>::type);
-    }
+    var(const char *name, const char *desc, const u::string &def);
+    var(const char *name, const char *desc, const u::string &def, command cb);
 
-    var(const char *name, const char *desc, const u::string &def, command cb)
-        : m_default(def)
-        , m_current(def)
-        , m_callback(cb)
-    {
-        void *self = reinterpret_cast<void *>(this);
-        varRegister(name, desc, self, varTypeTraits<u::string>::type);
-    }
-
-    operator u::string&() {
-        return m_current;
-    }
-
-    const u::string get() const {
-        return m_current;
-    }
-
-    void set(const u::string &value) {
-        m_current = value;
-    }
-
-    void operator()() {
-        if (m_callback)
-            m_callback(m_current);
-    }
+    operator u::string&();
+    const u::string get() const;
+    void set(const u::string &value);
+    void operator()();
 
 private:
     const u::string m_default;
     u::string m_current;
     command m_callback;
 };
+
+/// var<T>
+template <typename T>
+inline var<T>::var(const char *name,
+                   const char *desc,
+                   const T &min,
+                   const T &max,
+                   const T &def)
+    : m_min(min)
+    , m_max(max)
+    , m_default(def)
+    , m_current(def)
+    , m_callback(nullptr)
+{
+    void *self = reinterpret_cast<void *>(this);
+    varRegister(name, desc, self, varTypeTraits<T>::type);
+}
+
+template <typename T>
+inline var<T>::var(const char *name,
+                   const char *desc,
+                   const T &min,
+                   const T &max,
+                   const T &def,
+                   typename var<T>::command cb)
+    : m_min(min)
+    , m_max(max)
+    , m_default(def)
+    , m_current(def)
+    , m_callback(cb)
+{
+    void *self = reinterpret_cast<void *>(this);
+    varRegister(name, desc, self, varTypeTraits<T>::type);
+}
+
+template <typename T>
+inline var<T>::operator T&() {
+    return m_current;
+}
+
+template <typename T>
+inline const T var<T>::get() const {
+    return m_current;
+}
+
+template <typename T>
+inline void var<T>::set(const T &value) {
+    if (value < m_min) return;
+    if (value > m_max) return;
+    m_current = value;
+}
+
+template <typename T>
+inline void var<T>::operator()() {
+    if (m_callback)
+        m_callback(m_current);
+}
+
+/// var<u::string>
+inline var<u::string>::var(const char *name,
+                           const char *desc,
+                           const u::string &def)
+    : m_default(def)
+    , m_current(def)
+    , m_callback(nullptr)
+{
+    void *self = reinterpret_cast<void *>(this);
+    varRegister(name, desc, self, varTypeTraits<u::string>::type);
+}
+
+inline var<u::string>::var(const char *name,
+                           const char *desc,
+                           const u::string &def,
+                           typename var<u::string>::command cb)
+    : m_default(def)
+    , m_current(def)
+    , m_callback(cb)
+{
+    void *self = reinterpret_cast<void *>(this);
+    varRegister(name, desc, self, varTypeTraits<u::string>::type);
+}
+
+inline var<u::string>::operator u::string&() {
+    return m_current;
+}
+
+inline const u::string var<u::string>::get() const {
+    return m_current;
+}
+
+inline void var<u::string>::set(const u::string &value) {
+    m_current = value;
+}
+
+inline void var<u::string>::operator()() {
+    if (m_callback)
+        m_callback(m_current);
+}
 
 bool writeConfig();
 bool readConfig();
