968a4f268362ff8d665bc8d434e8ec9b809104d7
Dale Weiler <weilercdale@gmail.com>
2014-10-26 03:07:02 -0400

    use GL_UNSIGNED_SHORT as the data format when dealing with RG textures

--- r_texture.cpp
+++ r_texture.cpp
@@ -30,8 +30,8 @@ namespace r {
 #endif
 #define R_TEX_DATA_RGB       GL_UNSIGNED_BYTE
 #define R_TEX_DATA_BGR       GL_UNSIGNED_BYTE
-#define R_TEX_DATA_RG        GL_UNSIGNED_BYTE
 #define R_TEX_DATA_LUMINANCE GL_UNSIGNED_BYTE
+#define R_TEX_DATA_RG        GL_UNSIGNED_SHORT
 
 static const unsigned char kTextureCacheVersion = 0xFA;
 
