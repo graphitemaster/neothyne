bc3f5194ff80a5902122d4192821114bd2f61b90
Dale Weiler <weilercdale@gmail.com>
2015-12-18 00:18:36 -0500

    Smaller table

--- u_misc.cpp
+++ u_misc.cpp
@@ -204,7 +204,8 @@ float randf() {
 }
 
 unsigned char log2(uint32_t v) {
-    static const uint32_t kMultiplyDeBruijnBitPosition[32] = {
+    // 32-byte table
+    static const unsigned char kMultiplyDeBruijnBitPosition[32] = {
       0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8,
       31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
     };
@@ -212,5 +213,3 @@ unsigned char log2(uint32_t v) {
 }
 
 }
-
-#undef $
