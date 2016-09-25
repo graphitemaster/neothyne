ab81e2623ac6d97ad8cc38de4a7f175a6a0cd4b5
Dale Weiler <weilercdale@gmail.com>
2016-09-25 19:29:09 -0400

    Better tests

--- s_test.cpp
+++ s_test.cpp
@@ -8,44 +8,71 @@
 
 namespace s {
 
+static char script[] = R"(
+fn level0() {
+    let obj = {
+        a = 5,
+        b = null,
+        bar = mfn() {
+            print(this.a - this.b);
+        }
+    };
+
+    obj.b = 7;
+    obj["foo"] = mfn() {
+        print(this.a + this.b);
+    };
+
+    let a = 10;
+    while (a > 0) {
+        obj.foo();
+        obj.bar();
+        a = a - 1;
+    }
+}
+
+fn level1() { level0(); }
+fn level2() { level1(); }
+fn level3() { level2(); }
+
+let obj = {
+    a = level3
+};
+
+obj["a"]();
+
+print("2 != 2 => ", 2 != 2);
+print("2 !< 2 => ", 2 !< 2);
+print("2 !> 2 => ", 2 !> 2);
+print("2 !<= 2 => ", 2 !<= 2);
+print("2 !>= 2 => ", 2 !>= 2);
+
+print("string " + "concatenation");
+
+let A = { value = 1337 };
+let B = { value = A };
+let C = { value = B };
+let D = { value = C };
+
+print(D.value.value.value.value);
+print(D["value"]["value"]["value"]["value"]);
+
+// comment
+
+/* a multi
+   line comment
+   to test this */
+
+// make cycles to test GC
+let tree = { next = null, prev = null };
+tree.next = tree;
+tree.prev = tree;
+
+)";
+
 void test() {
     Object *root = createRoot();
-
-    char data[] =
-    "fn level0() {"
-    "  let obj = {"
-    "    a = 5,"
-    "    b = null,"
-    "    bar = mfn() {"
-    "      print(this.a - this.b);"
-    "    }"
-    "  };"
-    "  obj.b = 7;"
-    "  obj[\"foo\"] = mfn() { print(this.a + this.b); };"
-    "  let a = 10;"
-    "  while (a > 0) {"
-    "    obj.foo();"
-    "    obj.bar();"
-    "    a = a - 1;"
-    "  }"
-    "}"
-    "fn level1() { level0(); }"
-    "fn level2() { level1(); }"
-    "fn level3() { level2(); }"
-    "let obj = { a = level3 };"
-    "obj[\"a\"]();"
-    "print(\"2 != 2 => \", 2 != 2);"
-    "print(\"2 !< 2 => \", 2 !< 2);"
-    "print(\"2 !> 2 => \", 2 !> 2);"
-    "print(\"2 !<= 2 => \", 2 !<= 2);"
-    "print(\"2 !>= 2 => \", 2 !>= 2);"
-    "print(\"string \" + \"concatenation\");"
-    "let A = { value = 1337 };"
-    "let B = { value = A };"
-    "let C = { value = B };"
-    "let D = { value = C };"
-    "print(D.value.value.value.value);";
-    char *text = data;
+    char *text = script;
 
     UserFunction *module = Parser::parseModule(&text);
     module->dump();
