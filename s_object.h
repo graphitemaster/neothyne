172bea00de04a4d3aa74b806e015fd611f327383
Dale Weiler <weilercdale@gmail.com>
2016-12-07 16:02:42 +0000

    Document the scripting language types

--- s_object.h
+++ s_object.h
@@ -30,8 +30,13 @@ struct State;
 struct IntObject;
 
 struct Field {
+    // Name of the field
     const char *m_name;
+
+    // The length of the field name
     size_t m_nameLength;
+
+    // The field value
     void *m_value;
 };
 
@@ -42,8 +47,13 @@ struct Table {
 
     static void *lookup(Table *table, const char *key, size_t keyLength, bool *found);
 
+    // Array of fields
     Field *m_fields;
+
+    // The amount of fields allocated to the table
     size_t m_fieldsNum;
+
+    // The amount of fields stored
     size_t m_fieldsStored;
 };
 
@@ -75,27 +85,59 @@ struct Object {
     static Object *newMark(State *state);
     static Object *newClosure(Object *context, UserFunction *function);
 
+    // Objects are basically tables
     Table m_table;
+
+    // The parent object; all objects have parent objects except for the
+    // Object object which is the root of the object hiearchy.
     Object *m_parent;
+
+    // The size of the object
     size_t m_size;
+
+    // Flags associated with this object
     ObjectFlags m_flags;
+
+    // The previous object
     Object *m_prev;
+
+    // The GC mark function to mark this object during GC mark phase
     void (*m_mark)(State *state, Object *object);
 };
 
+// Doubly linked list of GC roots
 struct RootSet {
+    // Array of objects in this GC root
     Object **m_objects;
+
+    // The amount of objects in this GC root
     size_t m_count;
+
+    // Pointer to the previous GC root
     RootSet *m_prev;
+
+    // Pointer to the next GC root
     RootSet *m_next;
 };
 
+// Represents a call frame
 struct CallFrame {
+    // The function for this call frame
     UserFunction *m_function;
+
+    // The context of this function
     Object *m_context;
+
+    // Array of slots allocated for this call frame
     Object **m_slots;
+
+    // The amount of slots for this call frame
     size_t m_count;
+
+    // A GC root object for this call frame
     RootSet m_root;
+
+    // The list of instructions for this call frame
     Instruction *m_instructions;
 };
 
@@ -106,47 +148,97 @@ enum RunState {
 };
 
 struct GCState {
+    // The last GC root
     RootSet *m_tail;
+
+    // The last allocated object
     Object *m_lastObjectAllocated;
+
+    // The total number of objects allocated
     int m_numObjectsAllocated;
+
+    // When to issue the next GC cycle
     int m_nextRun;
+
+    // GC root of all permanent objects
     RootSet m_permanents;
+
+    // How many times the GC has been issued disabledness since the last
+    // cycle for some operation
     int m_disabledness;
+
+    // Indicates that a GC cycle was meant to be issued but the GC was disabled
+    // used to play catchup
     bool m_missed;
 };
 
+// The cache of all permanent objects
 struct ValueCache {
+    // [Bool]
     Object *m_boolFalse;
     Object *m_boolTrue;
+
+    // [Int]
     Object *m_intZero;
+
+    // Preallocated array of objects used to place function calls
     Object ***m_preallocatedArguments;
 };
 
 struct ProfileState {
+    // The last time we recorded a profile
     struct timespec m_lastTime;
+
+    // The next VM cycle to record another profile
     int m_nextCheck;
+
+    // Table encoding the amount of direct references to keys
     Table m_directTable;
+
+    // Table encoding the amount of indirect reference to key
     Table m_indirectTable;
+
     static void dump(SourceRange source, ProfileState *profileState);
 };
 
 struct SharedState {
+    // Garbage collector state
     GCState m_gcState;
+
+    // Profiling state
     ProfileState m_profileState;
+
+    // Cache of permanent objects
     ValueCache m_valueCache;
+
+    // The amount of VM cycles
     int m_cycleCount;
 };
 
 struct State {
+    // Parent VM state
     State *m_parent;
+
+    // State shared across multiple substates
     SharedState *m_shared;
+
+    // The stack for placing function calls
     CallFrame *m_stack;
+
+    // The length of the stack
     size_t m_length;
+
+    // The root object (literally Object's root)
     Object *m_root;
+
+    // Resulting value object. For instance function call return
     Object *m_resultValue;
+
+    // The running state of the VM
     RunState m_runState;
+
+    // Contains a non empty string if the VM encountered an error
     u::string m_error;
-    size_t m_count;
 };
 
 struct FunctionObject : Object {
